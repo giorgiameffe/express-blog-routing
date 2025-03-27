c60b7516fb6564fa7caf6a4306995d8428615a6d
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-27 16:20:57 +0100

    feat: add routes for CRUD operations

--- routers/posts.js
+++ routers/posts.js
@@ -3,3 +3,40 @@ const express = require('express');
 
 // funzione express.Router 
 const router = express.Router();
+
+// Creare rotte per operazioni CRUD
+
+// index
+router.get('/', function (req,res) {
+    res.send('Lista dei post');
+})
+
+// show 
+router.get('/:id', function (req,res) {
+    const {id} = req.params;
+    res.send(`Dettagli della pizza ${id}`);
+})
+
+// store
+router.post('/', function (req,res) { 
+    res.send('Creazione di un nuovo post');
+})
+
+// update 
+router.put('/:id', function (req,res) { 
+    const {id} = req.params;
+    res.send(`Modifica integrale del post ${id}`);
+})
+
+// modify 
+router.patch('/:id', function (req,res) {
+    const {id} = req.params;
+    res.send(`Modifica parziale del post ${id}`);
+})
+
+// destroy 
+router.delete('/:id', function(req,res) {
+    const {id} = req.params;
+    res.send(`Eliminazione del post ${id}`);
+})
+
